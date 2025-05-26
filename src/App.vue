65af396ed699f9dd8bed80781f978982cc2e55f4
Franco Valderrama <franyvalde@gmail.com>
2025-05-26 20:32:17 -0300

    update del parcial

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <h1>Proyecto Parcial</h1>
     <ModificadorDeTexto />
   </div>
+  <hr>
+  <p>Respuesta 1: la forma reducida de v-on:click es @click / Respuesta 2: el modificador trim recorta... Los espacios adelante y atras en una entrada de texto / Respuesta 3: La propiedad routes en la definición del router de Vue.js tiene como función... definir el path y el componente activo para esa ruta</p>
 </template>
 
 <script>
